9c872b9b04f98e5f089871acb27c3c3e4ded092e
Shane Kilkelly <shane@kilkelly.me>
2015-05-25 18:38:30 +0100

    Really primitive beginning of example node app.

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var BedquiltClient = require('bedquilt').BedquiltClient;
+
+var db = 'postgres://localhost/bedquilt_example';
 
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'BedquiltDB Example' });
+  BedquiltClient.connect(db, function(err, client) {
+    var items = client.collection('items');
+    items.find({}, function(err, result) {
+      res.render('index', {
+        title: 'BedquiltDB Example',
+        items: result
+      });
+    });
+  });
 });
 
 module.exports = router;
