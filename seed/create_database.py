114df0074f246a40b156375d0458978caf9a2e45
Shane Kilkelly <shane@kilkelly.me>
2015-05-26 19:35:45 +0100

    Improve the seed script

--- seed/create_database.py
+++ seed/create_database.py
@@ -2,10 +2,68 @@
 
 import os
 import subprocess
+import pipes
+import json
+
+
+BEDQUILT_EXAMPLE = 'bedquilt_example'
+
+
+def psql(sql, db='postgres'):
+    return subprocess.check_output([
+        'psql', '-qAt', '-d', db,
+        '-c', sql
+    ])
+
+    return result
+
+
+def sql_escape(st):
+    return st.replace("'", "''")
+
 
 def main():
-    print ">> Creating the bedquilt_example database"
 
+    print ">> Seeding {}...".format(BEDQUILT_EXAMPLE)
+
+    # Get list of existing databases
+    databases = psql(
+        "select datname from pg_catalog.pg_database"
+    ).splitlines()
+    databases = map(lambda s: s.strip(), databases)
+
+    # Check if the example database exists already
+    if BEDQUILT_EXAMPLE in databases:
+        print ">> Database {} already exists".format(BEDQUILT_EXAMPLE)
+        print ">> Dropping {}".format(BEDQUILT_EXAMPLE)
+        # TODO: handle client connections to database
+        psql("drop database {}".format(BEDQUILT_EXAMPLE))
+
+    # Create the bedquilt_example database
+    print ">> Creating the {} database".format(BEDQUILT_EXAMPLE)
+    psql("create database {}".format(BEDQUILT_EXAMPLE))
+
+    # Enable extensions on db
+    print ">> Enabling bedquilt extension on {}".format(BEDQUILT_EXAMPLE)
+    psql(
+        """
+        create extension if not exists pgcrypto;
+        drop extension if exists bedquilt;
+        create extension bedquilt;
+        """,
+        db=BEDQUILT_EXAMPLE)
+
+    # loop over data:
+    #   - insert into bedquilt_example
+    print ">> Adding data to database"
+    data = json.load(open('seed/data.json'))
+    tickets = data['tickets']
+    for ticket in tickets:
+        psql("""select bq_insert('tickets', '{}'::json)""".
+             format(sql_escape(json.dumps(ticket))),
+             db=BEDQUILT_EXAMPLE)
+
+    print ">> Done"
 
 if __name__ == '__main__':
     main()
